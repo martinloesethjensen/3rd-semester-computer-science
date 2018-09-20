6a2d9132787fa3f91a5014f693c7edbdca018205
Valhalla <martinfloorball1997@gmail.com>
2018-09-20 19:38:47 +0200

    Added getters and setters plus fields to classes.

--- src/CasinoCopenhagen/Game.java
+++ src/CasinoCopenhagen/Game.java
@@ -1,4 +1,79 @@
 package CasinoCopenhagen;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Game {
+	private Tournament tournament;
+
+	private ArrayList<Game> games = new ArrayList<>();
+	private ArrayList<User> players = new ArrayList<>();
+	private ArrayList<User> winners = new ArrayList<>();
+
+	private HashMap<Tournament, ArrayList<User>> playersOfTournaments = new HashMap<>();
+	private HashMap<Tournament, ArrayList<User>> winnersOfTournaments = new HashMap<>();
+
+	public Game() {}
+
+	public Game(Tournament tournament, ArrayList<Game> games, ArrayList<User> players, ArrayList<User> winners,
+	            HashMap<Tournament, ArrayList<User>> playersOfTournaments,
+	            HashMap<Tournament, ArrayList<User>> winnersOfTournaments)
+	{
+		this.tournament = tournament;
+		this.games = games;
+		this.players = players;
+		this.winners = winners;
+		this.playersOfTournaments = playersOfTournaments;
+		this.winnersOfTournaments = winnersOfTournaments;
+	}
+
+
+
+	public Tournament getTournament() {
+		return tournament;
+	}
+
+	public void setTournament(Tournament tournament) {
+		this.tournament = tournament;
+	}
+
+	public ArrayList<Game> getGames() {
+		return games;
+	}
+
+	public void setGames(ArrayList<Game> games) {
+		this.games = games;
+	}
+
+	public ArrayList<User> getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(ArrayList<User> players) {
+		this.players = players;
+	}
+
+	public ArrayList<User> getWinners() {
+		return winners;
+	}
+
+	public void setWinners(ArrayList<User> winners) {
+		this.winners = winners;
+	}
+
+	public HashMap<Tournament, ArrayList<User>> getPlayersOfTournaments() {
+		return playersOfTournaments;
+	}
+
+	public void setPlayersOfTournaments(HashMap<Tournament, ArrayList<User>> playersOfTournaments) {
+		this.playersOfTournaments = playersOfTournaments;
+	}
+
+	public HashMap<Tournament, ArrayList<User>> getWinnersOfTournaments() {
+		return winnersOfTournaments;
+	}
+
+	public void setWinnersOfTournaments(HashMap<Tournament, ArrayList<User>> winnersOfTournaments) {
+		this.winnersOfTournaments = winnersOfTournaments;
+	}
 }
--- src/CasinoCopenhagen/Tournament.java
+++ src/CasinoCopenhagen/Tournament.java
@@ -1,4 +1,65 @@
 package CasinoCopenhagen;
 
+import java.util.ArrayList;
+
 public class Tournament {
+	private String tournamentName;
+	private double prizeToWin;
+	private final double MIN_AMOUNT = 0.1;
+	private double amountToEnter = prizeToWin - (prizeToWin * MIN_AMOUNT);
+	private ArrayList<Tournament> tournaments = new ArrayList<>();
+
+	public Tournament() {
+	}
+
+	public Tournament(String tournamentName, double prizeToWin, double amountToEnter) {
+		this.tournamentName = tournamentName;
+		this.prizeToWin = prizeToWin;
+		this.amountToEnter = amountToEnter;
+	}
+
+	@Override
+	public String toString() {
+		return "Tournament{" +
+			"tournamentName='" + tournamentName + '\'' +
+			", prizeToWin=" + prizeToWin +
+			", amountToEnter=" + amountToEnter +
+			'}';
+	}
+
+	public ArrayList<Tournament> getTournaments() {
+		return tournaments;
+	}
+
+	public void setTournaments(ArrayList<Tournament> tournaments) {
+		this.tournaments = tournaments;
+	}
+
+	public String getTournamentName() {
+		return tournamentName;
+	}
+
+	public void setTournamentName(String tournamentName) {
+		this.tournamentName = tournamentName;
+	}
+
+	public double getPrizeToWin() {
+		return prizeToWin;
+	}
+
+	public void setPrizeToWin(double prizeToWin) {
+		this.prizeToWin = prizeToWin;
+	}
+
+	public double getMIN_AMOUNT() {
+		return MIN_AMOUNT;
+	}
+
+	public double getAmountToEnter() {
+		return amountToEnter;
+	}
+
+	public void setAmountToEnter(double amountToEnter) {
+		this.amountToEnter = amountToEnter;
+	}
 }
